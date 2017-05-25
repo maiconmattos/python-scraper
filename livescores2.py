11775dc669f52a12dfd7b7d81d962b8d4571a30b
Maicon Anderson Mattos da Silva <maiconmattos@Maicons-MacBook-Pro.local>
2017-05-25 18:53:15 +0100

    Adding a new scraping source

--- livescores2.py
+++ livescores2.py
@@ -12,13 +12,17 @@ for matchDiv in matchDivs :
     t1 = ''
     t2 = ''
     scoreDiv = matchDiv.find('div', class_="sco")
+    scores = scoreDiv.text.replace(" ", "").split("-")
+    scoreT1 = scores[0]
+    scoreT2 = scores[1]
+    clockDiv = matchDiv.find('div', class_="min")
     for teamDiv in teamDivs:
     	if 'tright' in teamDiv.attrs['class']:
            t1  = teamDiv
         else:
            t2  = teamDiv
 
-    print(t1.text + " " + scoreDiv.text + " " + t2.text)
+    print(clockDiv.text.strip() + " : "+ t1.text.strip() + "-" + scoreT1 + " X "  + scoreT2 + "-" + t2.text.strip())
     
     #t2 = matchDiv.find('div', class_="tright"
     #print(t1)
