8287ba16fd6b59ebd1268ff6cd40e9ec804bd522
Maicon Anderson Mattos da Silva <maiconmattos@Maicons-MacBook-Pro.local>
2017-05-25 15:37:17 +0100

    Adding code to save data to csv file

--- scraper.py
+++ scraper.py
@@ -1,5 +1,7 @@
 import requests
+import csv
 from BeatufilSoup import BeautifulSoup
+
 url = url = 'http://www.showmeboone.com/sheriff/JailResidents/JailResidents.asp'
 response = requests.get(url)
 html = response.content
@@ -7,6 +9,14 @@ html = response.content
 soup = BeautifulSoup(html)
 table = soup.find('tbody', attrs={'class' : 'stripe'})
 
+rowsList = []
 for row in table.findAll('tr')
+  cellsList = []
   for cell in row.findAll('td')
-    print cell.text.replace('&nbsp;', '')
+    text = cell.text.replace('&nbsp;', '')
+    cellsList.append(text)
+  rowsList.append(cellsList)
+
+outfile = open("./inmates.csv", "wb")
+writer = csv.writer(outfile)
+writer.writeRows(rowsList)
