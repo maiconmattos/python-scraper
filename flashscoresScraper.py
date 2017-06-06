450f726ddcd24bbee0a92069bc0ee9bd7ec6a3a7
maiconmattos <maiconmattos@172.16.24.230>
2017-06-06 16:56:43 +0100

    Adjusting livescores scrapper to display only live games

--- flashscoresScraper.py
+++ flashscoresScraper.py
@@ -1,12 +1,9 @@
 import requests
-import csv
 from BeautifulSoup import BeautifulSoup
 
 url = 'http://www.flashscore.com/soccer/'
-url2 = "http://www.xscores.com/soccer/livescores/25-05"
-response = requests.get(url2)
+response = requests.get(url)
 html = response.content
 
 soup = BeautifulSoup(html)
-with open("xshtml.html", "w") as file:
-    file.write(str(soup))
+with open("xshtml.html", "w") as f: f.write(str(soup))
--- livescores2.py
+++ livescores2.py
@@ -7,18 +7,17 @@ html = response.content
 soup = BeautifulSoup(html, 'html.parser')
 matchDivs = soup.find_all('div', class_='row-gray')
 for matchDiv in matchDivs :
-    teamDivs = matchDiv.findAll('div', class_="ply")
-    t1 = ''
-    t2 = ''
-    scoreDiv = matchDiv.find('div', class_="sco")
-    scores = scoreDiv.text.replace(" ", "").split("-")
-    scoreT1 = scores[0]
-    scoreT2 = scores[1]
     clockDiv = matchDiv.find('div', class_="min")
-    for teamDiv in teamDivs:
-    	if 'tright' in teamDiv.attrs['class']:
-           team1  = teamDiv.text.strip()
-        else:
-           team2  = teamDiv.text.strip()
-
-    print(clockDiv.text.strip() + " : "+ team1 + "-" + scoreT1 + " X "  + scoreT2 + "-" + team2)
+    isLive = clockDiv.find('img')
+    if isLive :
+        clock = clockDiv.text.strip()
+        scoreDiv = matchDiv.find('div', class_="sco")
+        scores = scoreDiv.text.replace(" ", "").split("-")
+        scoreT1 = scores[0]
+        scoreT2 = scores[1]
+        teamDivs = matchDiv.findAll('div', class_="ply")
+        for teamDiv in teamDivs:
+            if 'tright' in teamDiv.attrs['class']:team1  = teamDiv.text.strip()
+        else: 
+            team2  = teamDiv.text.strip()
+        print(clock + " : "+ team1 + "-" + scoreT1 + " vs "  + scoreT2 + "-" + team2)
