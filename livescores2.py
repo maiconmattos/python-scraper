ce72e11153f17f5cdcb2b6c2608f309330e2135c
maiconmattos <maiconmattos@172.16.24.230>
2017-06-06 17:32:58 +0100

    Adding code to scrape the data in scheduled time

--- livescores2.py
+++ livescores2.py
@@ -1,23 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
+import sched, time
+s = sched.scheduler(time.time, time.sleep)
+def print_time(): print "From print_time", time.time()
 
-url = "http://livescores.com"
-response = requests.get(url)
-html = response.content
-soup = BeautifulSoup(html, 'html.parser')
-matchDivs = soup.find_all('div', class_='row-gray')
-for matchDiv in matchDivs :
-    clockDiv = matchDiv.find('div', class_="min")
-    isLive = clockDiv.find('img')
-    if isLive :
-        clock = clockDiv.text.strip()
-        scoreDiv = matchDiv.find('div', class_="sco")
-        scores = scoreDiv.text.replace(" ", "").split("-")
-        scoreT1 = scores[0]
-        scoreT2 = scores[1]
-        teamDivs = matchDiv.findAll('div', class_="ply")
-        for teamDiv in teamDivs:
-            if 'tright' in teamDiv.attrs['class']:team1  = teamDiv.text.strip()
-        else: 
-            team2  = teamDiv.text.strip()
-        print(clock + " : "+ team1 + "-" + scoreT1 + " vs "  + scoreT2 + "-" + team2)
+def scrapeResults() :
+    url = "http://livescores.com"
+    response = requests.get(url)
+    html = response.content
+    soup = BeautifulSoup(html, 'html.parser')
+    matchDivs = soup.find_all('div', class_='row-gray')
+    for matchDiv in matchDivs :
+        clockDiv = matchDiv.find('div', class_="min")
+        isLive = clockDiv.find('img')
+        if isLive :
+            clock = clockDiv.text.strip()
+            scoreDiv = matchDiv.find('div', class_="sco")
+            scores = scoreDiv.text.replace(" ", "").split("-")
+            scoreT1 = scores[0]
+            scoreT2 = scores[1]
+            teamDivs = matchDiv.findAll('div', class_="ply")
+            for teamDiv in teamDivs:
+                if 'tright' in teamDiv.attrs['class']:team1 = teamDiv.text.strip()
+            else: 
+                team2 = teamDiv.text.strip()
+            print(clock + " : " + team1 + "-" + scoreT1 + " vs " + scoreT2 + "-" + team2)
+    print('-----------------------------')
+
+while(True) :
+    s.enter(10, 1, scrapeResults, ())
+    s.run()
